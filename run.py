9020b26965dcfc8737cb8c1715cf8cba2fc0d1d4
Eunhak Lee <admin@return0927.xyz>
2020-04-22 19:16:48 +0900

    Mod: Fetch bot token from ENV value

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from discord.ext.commands import AutoShardedBot as Bot
+import os
 
 
 bot = Bot(command_prefix="e;r ")
 bot.load_extension("cogs.reaction_role")
 
-bot.run("")
+bot.run(os.environ["DISCORD_ACCESS_TOKEN"])
